abfe110e0ddaecd3a00cfa0a20a145e769aeaa93
shamsheer97 <mdsray2@gmail.com>
2022-02-06 11:22:02 +0530

    hi

---
+++ src/org/datype/DataType.java
@@ -0,0 +1,30 @@
+package org.datype;
+
+public class DataType {
+public static void main(String[] args) {
+	byte birthDate = 02;
+	short birthYear = 1997;
+	int annualSalary = 300000000;
+	long yearlyTurnover = 889867867564l;
+	float sharePrice = 4453.9876578f;
+	float peRatio = 20/30f;
+	double yearlySalary = 683.4567897654d;
+	double monthExpanses = 30/90d;
+	char companyPerformance = 'g';
+	String companyName = "INFO TECH";
+	Boolean performanceGood = true;
+	
+	System.out.println("kumar birth date is"+birthDate);
+	System.out.println("kumar birth is" + birthYear);
+	System.out.println("kumar salary is"+annualSalary);
+	System.out.println("kumar company yearly turnover is"+yearlyTurnover);
+	System.out.println("kumar company sharePrice is"+ sharePrice);
+	System.out.println("kumar company ratio is"+peRatio);
+	System.out.println("kumar company emp salary is"+yearlySalary);
+	System.out.println("kumar comapny expanses is"+ monthExpanses);
+	System.out.println("kumar company performance is"+ companyPerformance);
+	System.out.println("kumar comapny name is"+companyName);
+	System.out.println("kumar company perform is good"+performanceGood);
+}
+}
+ 
